f645219dcba8e338d50fb2c3b91b2696eade42a4
yangchangjia <yangchangjia@qianxin.com>
2021-12-21 16:49:50 +0800

    add pressed file to look

--- utils/files.py
+++ utils/files.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 
 
@@ -13,6 +14,62 @@ class FileUtils:
                     result.append(str(file.absolute()))
             elif p.is_file():
                 result.append(str(file.absolute()))
+        return result
+
+    @staticmethod
+    def list_compressed_file(source_file: str):
+        result = []
+        if source_file.endswith(".tar.gz"):
+            des_dir = source_file[:source_file.rfind(".tar.gz")] + "/"
+            os.makedirs(des_dir)
+            cmd = "tar -zxvf {} -C {}".format(source_file, des_dir)
+            rs = os.system(cmd)
+            if not rs == 0:
+                return result
+            for root, dirs, files in os.walk(des_dir):
+                for file_name in files:
+                    result.extend(FileUtils.list_compressed_file(os.path.join(root, file_name)))
+        elif source_file.endswith(".tar"):
+            des_dir = source_file[:source_file.rfind(".tar")] + "/"
+            os.makedirs(des_dir)
+            cmd = "tar -xvf {} -C {}".format(source_file, des_dir)
+            rs = os.system(cmd)
+            if not rs == 0:
+                return result
+            for root, dirs, files in os.walk(des_dir):
+                for file_name in files:
+                    result.extend(FileUtils.list_compressed_file(os.path.join(root, file_name)))
+        elif source_file.endswith(".tar.bz2"):
+            des_dir = source_file[:source_file.rfind(".tar.bz2")] + "/"
+            os.makedirs(des_dir)
+            cmd = "tar -xjvf {} -C {}".format(source_file, des_dir)
+            rs = os.system(cmd)
+            if not rs == 0:
+                return result
+            for root, dirs, files in os.walk(des_dir):
+                for file_name in files:
+                    result.extend(FileUtils.list_compressed_file(os.path.join(root, file_name)))
+        elif source_file.endswith(".tar.Z"):
+            des_dir = source_file[:source_file.rfind(".tar.Z")] + "/"
+            os.makedirs(des_dir)
+            cmd = "tar -xZvf {} -C {}".format(source_file, des_dir)
+            rs = os.system(cmd)
+            if not rs == 0:
+                return result
+            for root, dirs, files in os.walk(des_dir):
+                for file_name in files:
+                    result.extend(FileUtils.list_compressed_file(os.path.join(root, file_name)))
+        elif source_file.endswith(".zip"):
+            des_dir = source_file[:source_file.rfind(".zip")] + "/"
+            cmd = "unzip -o {} -d {}".format(source_file, des_dir)
+            rs = os.system(cmd)
+            if not rs == 0:
+                return result
+            for root, dirs, files in os.walk(des_dir):
+                for file_name in files:
+                    result.extend(FileUtils.list_compressed_file(os.path.join(root, file_name)))
+        else:
+            result.append(source_file)
         return result
 
 
